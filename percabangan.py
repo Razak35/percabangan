0ec19f4c220fbe38bfa2963790b471809019f143
RazakRWID <abdulrazak894@gmail.coom>
2025-06-10 00:01:47 -0700

    Latihan awal percabangan

--- percabangan.py
+++ percabangan.py
@@ -1,12 +1,20 @@
 # percabangan
 
 jumlah_botol_susu = 173
+jumlah_telur = 589
+print(f"jumlah botol susu", {jumlah_botol_susu}, "btl")
+print(f"jumlah telur", {jumlah_telur}, "btr")
 if jumlah_botol_susu > 0 :
     print("Budi mengecek harganya, dan ternyata cukup")
-    print("Budi membeli 1 botol susu")
+    if jumlah_telur > 0 :
+        print("Budi membeli 1 botol susu")
+        print("Budi membeli 6 butir telur")
+    else:
+        print("Budi membeli 1 botol susu dan tidak membeli telur")
+
 
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 
 print("Budi pulang ke rumah")
-print("Budi menyerahkannya pada Ibu")
+print("Budi menyerahkan belanjaannya pada Ibu")
